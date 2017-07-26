6c6313623af0c32cf33c43a244c56f7610806b2a
pniedzielski <pniedzielski@PUKISW090.polsatc>
2017-07-26 16:49:44 +0200

    Zapis i odczyt z plików płaskich

--- helloworld/src/helloworld/ZapisDoPlikow.java
+++ helloworld/src/helloworld/ZapisDoPlikow.java
@@ -1,14 +1,23 @@
 package helloworld;
 
+//ODCZYT
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
+//ZAPIS
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class ZapisDoPlikow {
-	public static void main(String[] args) throws FileNotFoundException{
-	      File file = new File("plikplaski.txt");
-	      Scanner in = new Scanner(file);
-	      String zdanie = in.nextLine();
-	      System.out.println(zdanie);
-}
+		  public static void main(String[] args) throws FileNotFoundException{
+		      //ZAPIS
+			  PrintWriter zapis = new PrintWriter("PlikPlaski.txt");
+		      zapis.println("Ala ma kota, a kot ma Alę");
+		      zapis.close();
+		      
+		      //ODCZYT
+		      File file = new File("PlikPlaski.txt");
+		      Scanner in = new Scanner(file);
+		      String zdanie = in.nextLine();
+		      System.out.println(zdanie);
+	   }
 }
