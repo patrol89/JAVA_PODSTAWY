47fd67a19d1d954e57d33628644aa4c1cec2baf3
Patryk <Patryk@Patrol-PC>
2017-07-27 22:21:40 +0200

    Zakres widoczności zmiennych - 3 przykłady

--- helloworld/src/helloworld/ZakresWidocznosciZmiennych.java
+++ helloworld/src/helloworld/ZakresWidocznosciZmiennych.java
@@ -1,14 +1,33 @@
 package helloworld;
 
-public class ZakresWidocznosciZmiennych {
-	void metoda(){
-	      if(0==1){
-	         int a=5;
-	      }
-	   //tutaj zmienna "a" jest niewidoczna
-	 
-	  for(int i=0; i<10; i++){  //zmienna obowiÄ…zuje tylko w pÄ™tli
-	    System.out.println("Java");
-	  }
+//Zmienne "globalne" w obrêbie klasy
+	class JakasKlasa{
+		 int zmienna_globalna = 5;
+		 
+		 void metoda1(){
+		    tutaj mo¿esz u¿yæ zmiennej globalnej
+		 }
 	}
-}
+	
+//Zmienne "lokalne" w ramach metod
+	class JakasKlasa2{
+		  void metoda(){
+		      int zmienna_lokalna = 3;
+		  }
+		 
+		  int zmienna_globalna = zmienna_lokalna; //B£¥D!
+		}
+	
+//Dodatkowo zmienne lokalne mo¿na deklarowaæ nawet
+//wewn¹trz instrukcji steruj¹cych, lub jako licznik w pêtli
+	class JakasKlasa3{
+		  void metoda(){
+		      if(warunek){
+		         int a=5;
+		      }
+		   //tutaj zmienna "a" jest niewidoczna
+		 
+		  for(int i=0; i<10; i++){  //zmienna obowi¹zuje tylko w pêtli
+		    System.out.println("Java");
+		  }
+		}
