454417e3094b42992c31b0b57d083708ae1861cb
pniedzielski <pniedzielski@PUKISW090.polsatc>
2017-07-27 10:19:07 +0200

    Pętle while, do ... while, for

--- helloworld/src/helloworld/Petle.java
+++ helloworld/src/helloworld/Petle.java
@@ -7,8 +7,8 @@ public class Petle {
 	  	gdzie zakładana ilość powtórzeń jest bliżej nieokreślona, 
 	  	ale znamy warunek jaki musi być spełniony
 	  	*/
-		 int licznik = 0;
-		 while(licznik<10){
+		  int licznik = 0;
+		  while(licznik<10){
 	          System.out.println("To jest petla"+licznik);
 	          licznik++;
 	      }
@@ -31,12 +31,9 @@ public class Petle {
 	       * Pętlę for od dwóch poprzednich odróżnia przede wszystkim 
 	       * rodzaj zastosowań. W jej przypadku zazwyczaj wiemy dokładnie ile razy ma się jakaś czynność powtórzyć.     
 	       */
-	          
-	          
+	       for(int i=0; i<10; i++){
+	    	   System.out.println("To jest pętla");
+	       }
+	       System.out.println("Koniec pętli");
 	  }
-	
-	
-	
-	
-	
 }
